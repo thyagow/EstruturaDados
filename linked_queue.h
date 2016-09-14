7649537e408adb0c64d2aaa2f22299570b94f40a
thyagow <thiagobolgt@hotmail.com>
2016-09-13 21:51:32 -0300

    Add files via upload

--- linked_queue.h
+++ linked_queue.h
@@ -5,59 +5,61 @@
 #include <stdexcept>
 
 namespace structures {
-
+//! fila encadeada
+/*  implementação de fila através de nodos, não possui
+    um tamanho máximo definido
+*/
 
 template<typename T>
 
 class LinkedQueue {
-
-public:
-    
-    LinkedQueue(){
-        head = new Node(0u);
-        tail = new Node(0u);
+ public:
+    LinkedQueue() {  //! construtor
+        head = NULL;
+        tail = NULL;
         size_ = 0u;
-    }
+    }  // construtor padrão
 
-    ~LinkedQueue(){
+    ~LinkedQueue() {  //! destrutor
         clear();
-    }
+        delete(head);
+    }  // destrutor padrão
 
-    void clear(){ // limpar
-        while(!empty()){
+    void clear() {  //! limpar
+        while (!empty()) {
             dequeue();
         }
-    } 
+    }  // limpa a fila
 
-    void enqueue(const T& data){ // enfilerar
+    void enqueue(const T& data) {  //! enfilerar
         Node *run;
         Node *new_ = new Node(data);
-        
-        if(new_ == NULL)
+
+        if (new_ == NULL) {
             throw std::out_of_range("LISTACHEIAenqueue");
-        else{
-            if(!empty()){
+        } else {
+            if (!empty()) {
                 run = head;
-            
-                for(auto i = 0; i < size()-1; i++)
+
+                for (auto i = 0; i < size()-1; i++) {
                     run = run->next();
-                    
+                }
                 run->next(new_);
-            }else
+            } else {
                 head = new_;
-                
+            }
             tail = new_;
             size_++;
         }
-    } 
+    }  // adiciona um elemento à fila
 
-    T dequeue(){ // desenfilerar
+    T dequeue() {  //! desenfilerar
         Node *destroy;
         T back;
-        
-        if(empty())
+
+        if (empty()) {
             throw std::out_of_range("LISTAVAZIAdequeue");
-        else{
+        } else {
             destroy = head;
             back = destroy->data();
             head = destroy->next();
@@ -65,72 +67,66 @@ public:
             delete(destroy);
         }
         return back;
-    } 
+    }  // retira um elemento da fila
 
-    T& front() const{ // primeiro dado
-        if(empty())
+    T& front() const {  //! primeiro dado
+        if (empty())
             throw std::out_of_range("LISTAVAZIAfront");
         else
             return head->data();
-    } 
+    }  // mostra o primeiro elemento da fila
 
-    T& back() const{ // último dado
-        if(empty())
+    T& back() const {  //! último dado
+        if (empty())
             throw std::out_of_range("LISTAVAZIAback");
         else
             return tail->data();
-    }
+    }  // mostra o último elemento da fila
 
-    bool empty() const{ // fila vazia
-        if(size_ == 0)
+    bool empty() const {  //! fila vazia
+        if (size_ == 0)
             return true;
         else
             return false;
-    } 
+    }  // verifica se a fila está vazia
 
-    std::size_t size() const{ // tamanho
+    std::size_t size() const {  //! tamanho
         return size_;
-    } 
+    }  // verifica tamanho atal da fila
 
-private:
-
+ private:
     class Node {
-        
-        public: 
-            Node(const T& data):
-                data_{data}
-            {}
-            
-            Node(const T& data, Node *next):
-                data_{data},
-                next_{next}
-            {}
-        
-            T& data(){
-                return data_;
-            }
-            T& data() const{
-                return data_;
-            }
-            Node* next(){
-                return next_;
-            }
-            Node* next() const{
-                return next_;
-            }
-            void next(Node *next){
-                next_ = next;
-            }
-        
-        private:
-            T data_;
-            Node *next_{nullptr};
+     public:
+        Node(const T& data):
+            data_{data}
+        {}
+        Node(const T& data, Node *next):
+            data_{data},
+            next_{next}
+        {}
+        T& data() {
+            return data_;
+        }
+        T& data() const {
+            return data_;
+        }
+        Node* next() {
+            return next_;
+        }
+        Node* next() const {
+            return next_;
+        }
+        void next(Node *next) {
+            next_ = next;
+        }
+
+     private:
+        T data_;
+        Node *next_{nullptr};
     };
-    Node* head; // nodo-cabeça
-    Node* tail; // nodo-fim
-    std::size_t size_; // tamanho
+    Node* head;  // nodo-cabeça
+    Node* tail;  // nodo-fim
+    std::size_t size_;  // tamanho
 };
-
-}
-
+}  // namespace structures
 #endif
--- linked_stack.h
+++ linked_stack.h
@@ -5,84 +5,86 @@
 #include <stdexcept>
 
 namespace structures {
+//! pilha encadeada
+/*  pilha implementada através de nodos, não possui
+    tamanho máximo definido
+*/
 
 template<typename T>
 class LinkedStack {
-public:
-    
-    LinkedStack(){
+ public:
+    LinkedStack() {  //! construtor
         top_ = new Node(0u);
         size_ = 0u;
-    }
+    }  // construtor padrão
 
-    ~LinkedStack(){
+    ~LinkedStack() {  //! destrutor
         clear();
         delete(top_);
-    }
+    }  // destrutor padrão
 
-    void clear(){ // limpa pilha
-        while(!empty()){
+    void clear() {  //! limpa pilha
+        while (!empty()) {
             pop();
         }
-    } 
+    }  // limpa a pilha
 
-    void push(const T& data){ // empilha
+    void push(const T& data) {  //! empilha
         Node *new_ = new Node(data);
-        
-        if(new_ == NULL)
+
+        if (new_ == NULL) {
             throw std::out_of_range("LISTACHEIApush");
-        else{
-            if(!empty())
+        } else {
+            if (!empty())
                 new_->next(top_->next());
             top_->next(new_);
             size_++;
         }
-    } 
+    }  // adiciona um elemento à pilha
 
-    T pop(){ // desempilha
+    T pop() {  //! desempilha
     Node *destroy;
     T back;
-        if(empty())
+
+        if (empty()) {
             throw std::out_of_range("LISTAVAZIApop");
-        else{
-            if(size_ > 1){
+        } else {
+            if (size_ > 1) {
                 destroy = top_->next();
                 back = destroy->data();
                 top_->next(destroy->next());
                 size_--;
                 delete(destroy);
-            }else{
+            } else {
                 back = top_->next()->data();
                 size_--;
                 delete(top_->next());
             }
         }
         return back;
-    } 
+    }  // retira um elemento da pilha
 
-    T& top() const{ // dado no topo
-        if(empty())
+    T& top() const {  //! dado no topo
+        if (empty())
             throw std::out_of_range("LISTAVAZIAtop");
         else
             return top_->next()->data();
-    } 
+    }  // verifica o elemento no topo da pilha
 
-    bool empty() const{ // pilha vazia
-        if(size_ == 0)
+    bool empty() const {  //! pilha vazia
+        if (size_ == 0)
             return true;
         else
             return false;
-    } 
+    }  // verifica se a pilha está vazia
 
-    std::size_t size() const{ // tamanho da pilha
+    std::size_t size() const {  //! tamanho da pilha
         return size_;
-    } 
+    }  // verifica o tamanho atual da pilha
 
-private:
-    
+ private:
     class Node {
-    public:
-        
+     public:
         Node(const T& data):
             data_{data}
         {}
@@ -91,30 +93,31 @@ private:
             next_{next}
         {}
 
-        T& data(){ // getter: info
+        T& data() {  // getter: info
             return data_;
-        } 
-        const T& data() const{ // getter-constante: info
+        }
+        const T& data() const {  // getter-constante: info
             return data_;
-        } 
-        Node* next(){ // getter: próximo
+        }
+        Node* next() {  // getter: próximo
             return next_;
-        } 
-        const Node* next() const{ // getter-constante: próximo
+        }
+        const Node* next() const {  // getter-constante: próximo
             return next_;
-        } 
-        void next(Node* next){ // setter: próximo
+        }
+        void next(Node* next) {  // setter: próximo
             next_ = next;
-        } 
-    private:
+        }
+
+     private:
         T data_;
         Node* next_;
     };
 
-    Node* top_; // nodo-topo
-    std::size_t size_; // tamanho
+    Node* top_;  // nodo-topo
+    std::size_t size_;  // tamanho
 };
 
-}
+}  // namespace structures
 
 #endif
